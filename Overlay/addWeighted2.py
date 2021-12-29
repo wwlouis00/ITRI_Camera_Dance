4ee1506e00b774c117cee67d1563a1a09009abe7
unknown <wwlouis00@gmail.com>
2021-12-29 18:08:08 +0800

    update 2021/1/29

--- Overlay/addWeighted2.py
+++ Overlay/addWeighted2.py
@@ -1,26 +1,26 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
 def nothing(x):
     pass
 
-img1 = cv2.imread('ROI.bmp')
-img2 = cv2.imread('well.png')
+img1 = cv.imread('ROI.bmp')
+img2 = cv.imread('well.png')
 # 創建一個黑色背景的窗口
 img = np.zeros((500,500,3), np.uint8)
-cv2.namedWindow('image')
+cv.namedWindow('image')
 
-cv2.createTrackbar('a','image',0,100,nothing)
+cv.createTrackbar('background transparency','image',0,100,nothing)
 
 
 while(1):
-    cv2.imshow('image',img)
-    k = cv2.waitKey(1) & 0xFF
+    cv.imshow('image',img)
+    k = cv.waitKey(1) & 0xFF
     if k == 27:
         break
-    r = cv2.getTrackbarPos('a','image')
+    r = cv.getTrackbarPos('background transparency','image')
     r=float(r)/100.0
 
-    img=cv2.addWeighted(img1,r,img2,1.0-r,0)
+    img=cv.addWeighted(img1,r,img2,1.0-r,0)
 
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
