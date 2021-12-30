24c07b4d61daf6f93dee1650cfe32cde76b8daa4
unknown <wwlouis00@gmail.com>
2021-12-30 18:20:18 +0800

    update 2021/12/30

--- 8-13.py
+++
@@ -1,7 +0,0 @@
-from pytube import YouTube
-
-url = 'https://www.youtube.com/watch?v=yZIummTz9mM&list=LL&index=5&ab_channel=LinkinPark'
-yt = YouTube(url)
-print("開始下載")
-yt.streams.first().download()
-print("下載完成")
