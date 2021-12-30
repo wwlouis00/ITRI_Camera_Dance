58b58dcf479f220d963075817d993d70d6993523
unknown <wwlouis00@gmail.com>
2021-12-30 17:50:30 +0800

    remove some fils

--- Qrcode/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-if not os.path.isdir('./xlsx_result'):
-    print("Directory 'para' does not exist.")
-    os.mkdir('./xlsx_result')
-    print("Directory 'para' is established.")
-if not os.path.isdir('./image'):
-    print("Directory 'result' does not exist.")
-    os.mkdir('./image')
-    print("Directory 'result' is established.")
