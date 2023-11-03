f4b19cd718ea2c7ef77e302d56fffd9536d08c22
wwlouis00 <wwlouis00@gmail.com>
2023-11-03 16:10:04 +0800

    update oo

--- Driverless cars/easy_pratice.py
+++
@@ -1,7 +0,0 @@
-import cv2 as cv
-img = cv.imread("./test.png") #讀取圖片
-gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY) #灰階處理 
-cv.imshow('Normal',img)    #顯示原始圖片
-cv.imshow('Gray',gray)
-cv.waitKey(0)
-cv.destroyAllWindows()
