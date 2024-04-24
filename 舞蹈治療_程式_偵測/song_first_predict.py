bb0a6413c8dc4b8152211e45a23b04df87a29572
wwlouis00 <wwlouis00@gmail.com>
2024-04-24 15:50:19 +0800

    Add files via upload

--- 舞蹈治療_程式_偵測/song_first_predict.py
+++ 舞蹈治療_程式_偵測/song_first_predict.py
@@ -3,6 +3,7 @@ import cv2 as cv
 from ultralytics import YOLO
 from datetime import datetime
 import numpy as np
+import os
 
 def calculate_iou(box1, box2):
     # Calculate intersection coordinates
@@ -23,89 +24,100 @@ def calculate_iou(box1, box2):
 
     return iou
 
-model = YOLO("model/pose_20240320_first.pt")
-model2 = YOLO("yolov8s.pt")
-print(model.names)
+def process_video(video_path, model_path1, model_path2, output_path):
+    model = YOLO(model_path1)
+    model2 = YOLO(model_path2)
+    print(model.names)
 
-timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
 
-# Open video file
-video_path = "dance_video/20240131\原始影片/testcam3_20240131105545.mp4"
-cap = cv.VideoCapture(video_path)
-cap2 = cv.VideoCapture(video_path)
+    # Open video file
+    cap = cv.VideoCapture(video_path)
+    cap2 = cv.VideoCapture(video_path)
 
-# Video resolution
-width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))    # Get the width of the frame
-height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))  # Get the height of the frame
+    # Video resolution
+    width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))    # Get the width of the frame
+    height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))  # Get the height of the frame
 
-# print("width:"+str(width)+"  height:"+str(height))
-fourcc = cv.VideoWriter_fourcc(*'MJPG') 
-out = cv.VideoWriter("result_first_song/"+str(timestamp)+'_predict.mp4', fourcc, 20.0, (width,  height))  # Create an empty video
-out2 = cv.VideoWriter("result_first_song/"+str(timestamp)+'_person.mp4', fourcc, 20.0, (width,  height))  # Create an empty video
-csv_file_path = 'result_first_song/第一首歌_'+str(timestamp)+'_results.csv'
-with open(csv_file_path, mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["Frame", "Person ID", "人的位置", "動作位置", "動作類別", "動作分數"])
-    
-    frame_number = 0
-    while True:
-        ret, frame = cap.read()
-        ret, frame2 = cap2.read()
-        if not ret:
-            print("End of video file reached")
-            break
+    os.makedirs(output_path, exist_ok=True)
+    # Set up video writer
+    fourcc = cv.VideoWriter_fourcc(*'MJPG') 
+    out = cv.VideoWriter(output_path + os.path.splitext(os.path.basename(video_path))[0] +'_第一首歌_' + str(timestamp) + '_predict.mp4', fourcc, 20.0, (width,  height))  # Create an empty video
+    out2 = cv.VideoWriter(output_path + os.path.splitext(os.path.basename(video_path))[0] + '_第一首歌_' + str(timestamp) + '_person.mp4', fourcc, 20.0, (width,  height))  # Create an empty video
+
+    csv_file_name = os.path.splitext(os.path.basename(video_path))[0] + '_第一首歌_' + str(timestamp)+ '_results.csv'
+    csv_file_path = os.path.join(output_path, csv_file_name)
+    with open(csv_file_path, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Frame", "Person ID", "人的位置", "動作位置", "動作類別", "動作分數"])
         
-        detect_params = model.predict(frame, device=0)
-        detect_params2 = model2.track(frame,classes=0, persist=True)
-        boxes = detect_params[0].boxes
-        boxes2 = detect_params2[0].boxes
-       
-        boxes2_xyxy = detect_params2[0].boxes.xyxy.cpu().numpy().astype(int)
-        ids2_id = detect_params2 [0].boxes.id.cpu().numpy().astype(int)
-
-        for box, id in zip(boxes2_xyxy, ids2_id):
-            cv.rectangle(frame2, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
-            cv.putText(
-                frame2,
-                f"Person: {id}",
-                (box[0], 50),
-                cv.FONT_HERSHEY_SIMPLEX,
-                0.5,
-                (0, 0, 255),
-                2,
-            )
-        for box2, id2 in zip(boxes2, ids2_id):
-            clsID = box2.cls.cpu().numpy()[0]
-            conf = box2.conf.cpu().numpy()[0]
-            bb = box2.xyxy.cpu().numpy()[0]
+        frame_number = 0
+        while True:
+            ret, frame = cap.read()
+            ret, frame2 = cap2.read()
+            if not ret:
+                print("End of video file reached")
+                break
             
-            # Find the closest box from model1
-            closest_box = None
-            closest_confidence = 0
-            closest_class = ""
-            for box1 in boxes:
-                iou = calculate_iou(box1.xyxy.cpu().numpy()[0], bb)
-                if iou > 0.5 and box1.conf.cpu().numpy()[0] > closest_confidence:
-                    closest_confidence = box1.conf.cpu().numpy()[0]
-                    closest_box = box1.xyxy.cpu().numpy()[0]
-                    closest_class = model.names[box1.cls.cpu().numpy()[0]]
-            
-            # Write data to CSV
-            if closest_box is not None:
-                writer.writerow([frame_number, id2, bb, closest_box, closest_class, closest_confidence])
+            detect_params = model.predict(frame, device=0)
+            detect_params2 = model2.track(frame, classes=0, persist=True)
+            boxes = detect_params[0].boxes
+            boxes2 = detect_params2[0].boxes
+           
+            boxes2_xyxy = detect_params2[0].boxes.xyxy.cpu().numpy().astype(int)
+            ids2_id = detect_params2[0].boxes.id.cpu().numpy().astype(int)
 
-            
-        annotated_frame = detect_params[0].plot()
+            for box, id in zip(boxes2_xyxy, ids2_id):
+                cv.rectangle(frame2, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
+                cv.putText(
+                    frame2,
+                    f"Person: {id}",
+                    (box[0], 50),
+                    cv.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (0, 0, 255),
+                    2,
+                )
+            for box2, id2 in zip(boxes2, ids2_id):
+                clsID = box2.cls.cpu().numpy()[0]
+                conf = box2.conf.cpu().numpy()[0]
+                bb = box2.xyxy.cpu().numpy()[0]
+                
+                # Find the closest box from model1
+                closest_box = None
+                closest_confidence = 0
+                closest_class = ""
+                for box1 in boxes:
+                    iou = calculate_iou(box1.xyxy.cpu().numpy()[0], bb)
+                    if iou > 0.5 and box1.conf.cpu().numpy()[0] > closest_confidence:
+                        closest_confidence = box1.conf.cpu().numpy()[0]
+                        closest_box = box1.xyxy.cpu().numpy()[0]
+                        closest_class = model.names[box1.cls.cpu().numpy()[0]]
+                
+                # Write data to CSV
+                if closest_box is not None:
+                    writer.writerow([frame_number, id2, bb, closest_box, closest_class, closest_confidence])
 
-        out.write(annotated_frame)
-        out2.write(frame2)
-        cv.imshow("first song",annotated_frame)
-        cv.imshow("first song person", frame2) # Add this line to show boxes2 in a separate window
-        if cv.waitKey(1) & 0xFF == ord("q"):
-            break
-        frame_number += 1
+                
+            annotated_frame = detect_params[0].plot()
 
-cap.release()
-out.release()
-out2.release()
-cv.destroyAllWindows()
+            out.write(annotated_frame)
+            out2.write(frame2)
+            cv.imshow(os.path.splitext(os.path.basename(video_path))[0]+"_first song", annotated_frame)
+            cv.imshow(os.path.splitext(os.path.basename(video_path))[0]+"_first song person", frame2) # Add this line to show boxes2 in a separate window
+            if cv.waitKey(1) & 0xFF == ord("q"):
+                break
+            frame_number += 1
+
+    # Release video capture and close display window
+    cap.release()
+    out.release()
+    out2.release()
+    cv.destroyAllWindows()
+
+# 使用範例
+video_path = "dance_video/20240131/原始影片/testcam2_20240131105545.mp4"
+model_path1 = "model/pose_20240322_first.pt"
+model_path2 = "yolov8s.pt"
+output_path = "result_first_song/"+os.path.splitext(os.path.basename(video_path))[0]+"/"
+process_video(video_path, model_path1, model_path2, output_path)
